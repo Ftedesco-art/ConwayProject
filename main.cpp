fe9f6d9afedbf6f34b537b6f3e11e3ef65c2d644
Fernando Tedesco <fernandotedesco123@gmail.com>
2025-06-20 21:10:53 -0300

    commit for ConwayProject's stable version 0.0.1

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//CONWAY PROJECT 0.1 (stable)
+//F.T 2025
+
+#include "functions.hpp"
+#undef main
+
+int main(int argc, char* argv[])
+{
+
+    Simulation conwayproject;
+    conwayproject.run();
+ 
+}
+
+
+
+
